90a36fa38d5e5e279f4c7bdb8fc9f0d667c4b5e2
Kanoknuch Klopfenstein <eve.klopfenstein@gmail.com>
2021-04-18 16:47:16 +0200

    change path back to home -> /

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact path='/PawsUp' component={Home} />
+        <Route exact path='/' component={Home} />
         {/* <Route exact path='/about-us' component={AboutUs} />
         <Route exact path='/contact-us' component={Contact} /> */}
         <Route />
